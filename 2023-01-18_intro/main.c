4e7f7271fac7163fa3ad0489caaf51111efa4024
Weide Alan <adw58@zoo.cs.yale.edu>
2023-01-20 10:54:37 -0500

    Close files, also get filename for output.

--- 2023-01-18_intro/main.c
+++ 2023-01-18_intro/main.c
@@ -5,11 +5,14 @@
 int main(int argc, char *argv[])
 {
     FILE *in = fopen(argv[1], "r");
+    FILE *out = fopen(argv[2], "w");
     int n;
     while (fscanf(in, "%d", &n) == 1)
     {
         long answer = factorial(n);
-        fprintf(stdout, "%ld\n", answer);
+        fprintf(out, "%ld\n", answer);
     }
+    fclose(in);
+    fclose(out);
     return 0;
 }
