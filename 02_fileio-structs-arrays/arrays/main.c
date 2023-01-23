db269b304cc13bf2927c50c93a9ac74773e38005
Weide Alan <adw58@zoo.cs.yale.edu>
2023-01-23 11:38:02 -0500

    Added Arrays folder

---
+++ 02_fileio-structs-arrays/arrays/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int n = atoi(argv[1]);
+    int nums[n];
+    for (size_t i = 0; i < n; i++) 
+    {
+        nums[i] = i;
+    }
+
+    int factorials[n];
+    for (int i = 0; i < n; i++) 
+    {
+
+    }
+    return 0;
+}
