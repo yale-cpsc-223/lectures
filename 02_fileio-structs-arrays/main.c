3090d70734ea6a5b709905db9541271f8f955fdb
alanweide <alan.d.weide@gmail.com>
2023-01-30 12:50:35 -0500

    Changed constant names from C# to N#

--- 02_fileio-structs-arrays/main.c
+++ 02_fileio-structs-arrays/main.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include "factorial.h"
 
-#define C1 0
-#define C2 11
-#define C3 8
-#define C4 27
-#define C5 35
-#define C6 1992
+#define N1 0
+#define N2 11
+#define N3 8
+#define N4 27
+#define N5 35
+#define N6 1992
 
 int main(int argc, char *argv[])
 {
-    int n = C1;
+    int n = N1;
     char name[] = {'A', 'W'};
     char *exe_name = argv[0];
     printf("[%s]\n", name);
