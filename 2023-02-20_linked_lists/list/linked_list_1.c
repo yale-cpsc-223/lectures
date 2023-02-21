00e488051bb6c519c96b944f0ee95abdeefd9de0
Weide Alan <adw58@zoo.cs.yale.edu>
2023-02-20 23:10:37 -0500

    End of lecture 2023-02-20.

--- 2023-02-20_linked_lists/list/linked_list_1.c
+++ 2023-02-20_linked_lists/list/linked_list_1.c
@@ -15,25 +15,78 @@ struct implementation
 
 list *list_create()
 {
-    return NULL;
+    list *lst = malloc(sizeof(list));
+    lst->head = NULL;
+    return lst;
 }
 
 int list_length(list *lst)
 {
-    return 0;
+    int length = 0;
+    node *n = lst->head;
+    while (n->next != NULL)
+    {
+        n = n->next;
+        length++;
+    }
+    return length;
 }
 
 void list_append(list *lst, int val)
 {
+    node *new = malloc(sizeof(node));
+    new->payload = val;
+    new->next = NULL;
+    if (lst->head == NULL)
+    {
+        lst->head = new;
+    }
+    else
+    {
+        node *tail_candidate = lst->head;
+        while (tail_candidate->next != NULL)
+        {
+            tail_candidate = tail_candidate->next;
+        }
+        tail_candidate->next = new;
+    }
 }
 
 int list_remove_last(list *lst)
 {
-    return 0;
+    if (/* list is long */ 1)
+    {
+        node *prev /* TODO */;
+        node *n = lst->head;
+        while (n->next != NULL)
+        {
+            n = n->next;
+        }
+        prev->next = NULL;
+        int ans = n->payload;
+        free(n);
+        return ans;
+    }
+    else
+    {
+        node *to_remove = lst->head;
+        lst->head = NULL;
+        int ans = to_remove->payload;
+        free(to_remove);
+        return ans;
+    }
 }
 
 void list_destroy(list *lst)
 {
+    node *_1 = lst->head;
+    while (_1 != NULL)
+    {
+        node *_2 = _1->next;
+        free(_1);
+        _1 = _2;
+    }
+    free(lst);
 }
 
 void list_print(list *lst, FILE *out)
